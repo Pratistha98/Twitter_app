e888ca1d1ab725c022371e774eeade75ff55daec
Pratistha Maharjan <pratisthamaharjan@gmail.com>
2020-10-14 23:56:05 -0400

    Posted Tweets and its working

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class TweetViewController: UIViewController {
 
+    @IBOutlet weak var tweetTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,6 +23,16 @@ class TweetViewController: UIViewController {
     }
     
     @IBAction func tweet(_ sender: Any) {
+        if(!tweetTextView.text.isEmpty){
+            TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: {
+                self.dismiss(animated: true, completion: nil)
+            }, failure: { (error) in
+                print("Error in tweets  \(error)")
+            })
+        }
+        else{
+            self.dismiss(animated: true, completion: nil)
+        }
     }
     /*
     // MARK: - Navigation
